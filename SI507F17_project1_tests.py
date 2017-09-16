28cbe4d54c68f0c8250d0c88d618f36d0eabcb12
Stella Choi <chsweet@umich.edu>
2017-09-15 22:45:39 -0400

    Fix some code violations

--- SI507F17_project1_tests.py
+++ SI507F17_project1_tests.py
@@ -1,14 +1,19 @@
-## Do not change import statements.
+# Do not change import statements.
 import unittest
 from SI507F17_project1_cards import *
 
-## Write your unit tests to test the cards code here.
-## You should test to ensure that everything explained in the code description file works as that file says.
-## If you have correctly written the tests, at least 3 tests should fail. If more than 3 tests fail, it should be because multiple of the test methods address the same problem in the code.
-## You may write as many TestSuite subclasses as you like, but you should try to make these tests well-organized and easy to read the output.
-## You should invoke the tests with verbosity=2 (make sure you invoke them!)
+# Write your unit tests to test the cards code here.
+# You should test to ensure that everything explained in the code
+# description file works as that file says.
+# If you have correctly written the tests, at least 3 tests should fail.
+# If more than 3 tests fail, it should be because multiple of the test
+# methods address the same problem in the code.
+# You may write as many TestSuite subclasses as you like, but you should try
+# to make these tests well-organized and easy to read the output.
+# You should invoke the tests with verbosity=2 (make sure you invoke them!)
 
 ###########
+
 
 class TestCard(unittest.TestCase):
     def setUp(self):
@@ -25,11 +30,13 @@ class TestCard(unittest.TestCase):
         for suit in self.sample_suits:
             for face_rank in self.sample_face_ranks:
                 self.sample_cards.append(Card(suit, face_rank))
-                self.expected_values.append([Card.suit_names[suit], Card.faces[face_rank], face_rank])
+                self.expected_values.append([Card.suit_names[suit],
+                                            Card.faces[face_rank], face_rank])
 
             for other_rank in self.sample_other_ranks:
                 self.sample_cards.append(Card(suit, other_rank))
-                self.expected_values.append([Card.suit_names[suit], other_rank, other_rank])
+                self.expected_values.append([Card.suit_names[suit],
+                                            other_rank, other_rank])
 
     def test_class_var(self):
         self.assertEqual(type(Card.suit_names), type([]))
@@ -41,13 +48,18 @@ class TestCard(unittest.TestCase):
 
     def test_init(self):
         for i in range(len(self.sample_cards)):
-            self.assertEqual(self.sample_cards[i].suit, self.expected_values[i][0])
-            self.assertEqual(str(self.sample_cards[i].rank), str(self.expected_values[i][1]))
-            self.assertEqual(self.sample_cards[i].rank_num, self.expected_values[i][2])
+            self.assertEqual(self.sample_cards[i].suit,
+                             self.expected_values[i][0])
+            self.assertEqual(str(self.sample_cards[i].rank),
+                             str(self.expected_values[i][1]))
+            self.assertEqual(self.sample_cards[i].rank_num,
+                             self.expected_values[i][2])
 
     def test_print(self):
         for i in range(len(self.sample_cards)):
-            self.assertEqual(str(self.sample_cards[i]), str(self.expected_values[i][1]) + " of " + self.expected_values[i][0])
+            exp_result = str(self.expected_values[i][1]) \
+                             + " of " + self.expected_values[i][0]
+            self.assertEqual(str(self.sample_cards[i]), exp_result)
 
     def tearDown(self):
         pass
